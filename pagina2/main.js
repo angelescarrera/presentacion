ac761b611a8281ebf30ece03692c173a7d1ab896
Angeles Carrera <carreraangeles385@gmail.com>
2025-07-29 13:37:53 -0800

    Renombro inicio.html a index.html para GitHub Pages

--- pagina2/main.js
+++ pagina2/main.js
@@ -29,10 +29,3 @@ window.addEventListener('load', function () {
   }
 });
 
-const toggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-toggle.addEventListener('click', () => {
-  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-});
-
